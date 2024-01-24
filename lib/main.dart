e6d04219fa3b09b41895b54cebe3900541e20231
Md. Al Abedin Wasee <mdalabedinwasee@gmail.com>
2024-01-24 19:36:53 +0600

    facebook clone

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  // ekhon amon ekta function nibo ja amar app take run korbe
-  runApp(const MyApp(name: "Wasee",));
+  runApp(const MyApp(
+    name: "Wasee",
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -12,18 +13,79 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 0, 48, 2),
-            leading: const Icon(Icons.menu, size: 30, color: Colors.grey),
-              actions: const [Icon(Icons.search, size: 30, color: Colors.grey)],
-                title: const Text("This is my first app",style: TextStyle(color: Colors.grey, fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  ),
-                backgroundColor:Colors.grey,
-              body: Center(
-            child: Text(name, style: const TextStyle(color: Color.fromARGB(255, 0, 52, 2), fontSize: 50),
+          backgroundColor: const Color.fromARGB(255, 20, 20, 20),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.add_circle_outline,
+                  size: 30, color: Colors.white),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.search, size: 30, color: Colors.white),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.maps_ugc, size: 30, color: Colors.white),
+              onPressed: () {},
+            ),
+          ],
+          title: const Text(
+            "facebook",
+            style: TextStyle(
+                color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
           ),
+        ),
+        backgroundColor: const Color.fromARGB(255, 20, 20, 20),
+        body: Column(
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(Icons.home, color: Color.fromARGB(255, 150, 150, 150)),
+                Icon(Icons.smart_display,
+                    color: Color.fromARGB(255, 150, 150, 150)),
+                Icon(Icons.groups, color: Color.fromARGB(255, 150, 150, 150)),
+                Icon(Icons.notifications,
+                    color: Color.fromARGB(255, 150, 150, 150)),
+                Icon(Icons.menu, color: Color.fromARGB(255, 150, 150, 150)),
+              ],
+            ),
+            SizedBox(
+              height: 70,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: const Image(
+                        image: AssetImage('assets/IMG_0974-01.jpeg'),
+                        height: 50,
+                        width: 50),
+                  ),
+                  SizedBox(
+                    width: 290,
+                    height: 45,
+                    child: OutlinedButton(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 20, 20, 20),
+                          side: const BorderSide(
+                              width: 1,
+                              color: Color.fromARGB(255, 150, 150, 150))),
+                      child: const Text(
+                        "What's on your mind?                                                                    ",
+                        style: TextStyle(fontSize: 15, color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
